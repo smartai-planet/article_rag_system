5230860bd723c7feb3bfd5ec76429c11bbf9dd4e
All About AI & LLM <callme-smartai@proton.me>
2026-07-20 18:27:51 +0200

    Minor edits to font styles

--- app.py
+++ app.py
@@ -251,14 +251,14 @@ def streamlit_app():
     st.set_page_config(page_title="Smart Research Article Analyzer", layout="wide")
     st.title("🧾 Smart Article: Document Analyzer with RAG Integration")
 
-    st.header("How can Smart Article help you? Just Upload your document(s) and Smart Article will; ")
+    st.badge("How can Smart Article help you? Just Upload your document(s) and Smart Article will; ")
     st.write()
-    st.subheader("- Analyze your document(s) and embed your document(s) in a vector store through series of structured processes")
-    st.subheader("- Provide semantic search & Maximal Marginal Relevance functionality through querying Smart Article, providing smart responses. Just ask whatever query, and you get a response!")
-    st.subheader("- Responses are entirely based on the uploaded document(s), references from what part of the document the responses was derived from is also provided")
-    st.subheader(f"- Provide the ability to crawl the web and provide up to 20 other articles related to the document(s) uploaded, with download links. Just use the keyword: {st.write(_papers_)}")
-    st.subheader(f"- Provide the ability to extract all images from the document(s) uploaded, providing a download button. Just use the keyword: {st.write(_images_)}")
-    st.subheader("- Provide the opportunity to choose between different Model and Embedding Architectures")
+    st.caption("- Analyze your document(s) and embed your document(s) in a vector store through series of structured processes")
+    st.caption("- Provide semantic search & Maximal Marginal Relevance functionality through querying Smart Article, providing smart responses. Just ask whatever query, and you get a response!")
+    st.caption("- Responses are entirely based on the uploaded document(s), references from what part of the document the responses was derived from is also provided")
+    st.caption("- Provide the ability to crawl the web and provide up to 20 other articles related to the document(s) uploaded, with download links. Just use the keyword: papers")
+    st.caption("- Provide the ability to extract all images from the document(s) uploaded, providing a download button. Just use the keyword: images")
+    st.caption("- Provide the opportunity to choose between different Model and Embedding Architectures")
        
 
     # Sidebar for model selection
