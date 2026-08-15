46a78e78dbaaacad4bda657f453c7998734be87b
All About AI & LLM <callme-smartai@proton.me>
2026-08-15 03:52:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -568,8 +568,9 @@ def streamlit_app():
                 # Optional: reset session id so a fresh temp folder is made
                 # if the user uploads more files in the same browser tab
     
-                doc_ids = st.session_state.collection.get()["ids"]
-                st.session_state.collection.delete(ids=doc_ids)
+                #doc_ids = st.session_state.collection.get()["ids"]
+                #st.session_state.collection.delete(ids=doc_ids)
+                
                 #st.session_state.collection.delete()
                 
                 del st.session_state["session_id"]
